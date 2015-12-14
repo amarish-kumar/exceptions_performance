03d3f42d82143ec9f6205cb679f7f6b3b66616a5
nickc-ci <nchamberlain@cambridge-intl.com>
2015-12-14 17:00:37 -0500

    First commit

---
+++ ExceptionsPerformance/ParsingPerformance.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ExceptionsPerformance {
+    // https://stackoverflow.com/questions/150114/parsing-performance-if-tryparse-try-catch
+    public class ParsingPerformance {
+        private readonly ITestOutputHelper _output;
+        public ParsingPerformance(ITestOutputHelper output) {
+            _output = output;
+        }
+        [Fact]
+        public void Time_string_parse() {
+            for (double i = 0; i < 1; i += .1) {
+                double errorRate = i; // 10% of the time our users mess up
+                int count = 50000; // 10000 entries by a user
+
+                TimeSpan trycatch = TimeTryCatch(errorRate, 1, count);
+                TimeSpan tryparse = TimeTryParse(errorRate, 1, count);
+
+                _output.WriteLine("trycatch: {0}", trycatch);
+                _output.WriteLine("tryparse: {0}", tryparse);
+                _output.WriteLine("slowdown: {0}", trycatch.Subtract(tryparse));
+                _output.WriteLine(Environment.NewLine);
+            }
+        }
+
+        /// <param name="errorRate">Rate of errors in user input</param>
+        /// <returns>Total time taken</returns>
+        public static TimeSpan TimeTryCatch(double errorRate, int seed, int count) {
+            Stopwatch stopwatch = new Stopwatch();
+            Random random = new Random(seed);
+            string bad_prefix = @"X";
+
+            stopwatch.Start();
+            for (int ii = 0; ii < count; ++ii) {
+                string input = random.Next().ToString();
+                if (random.NextDouble() < errorRate) {
+                    input = bad_prefix + input;
+                }
+                int value = 0;
+                try {
+                    value = Int32.Parse(input);
+                } catch (FormatException) {
+                    value = -1; // we would do something here with a logger perhaps
+                }
+            }
+            stopwatch.Stop();
+
+            return stopwatch.Elapsed;
+        }
+
+        /// <param name="errorRate">Rate of errors in user input</param>
+        /// <returns>Total time taken</returns>
+        public static TimeSpan TimeTryParse(double errorRate, int seed, int count) {
+            Stopwatch stopwatch = new Stopwatch();
+            Random random = new Random(seed);
+            string bad_prefix = @"X";
+
+            stopwatch.Start();
+            for (int ii = 0; ii < count; ++ii) {
+                string input = random.Next().ToString();
+                if (random.NextDouble() < errorRate) {
+                    input = bad_prefix + input;
+                }
+                int value = 0;
+                if (!Int32.TryParse(input, out value)) {
+                    value = -1; // we would do something here with a logger perhaps
+                }
+            }
+            stopwatch.Stop();
+
+            return stopwatch.Elapsed;
+        }
+    }
+
+}
---
+++ ExceptionsPerformance/XmlParsingExample.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ExceptionsPerformance {
+    public class XmlParsingExample {
+        private readonly ITestOutputHelper _output;
+        public XmlParsingExample(ITestOutputHelper output) {
+            _output = output;
+        }
+
+        [Fact]
+        public void Build_sample_data() {
+            double errorRate = .1; // 10% of the time our users mess up
+            int count = 50000; // 10000 entries by a user
+
+            var doc = BuildSampleData(errorRate, count);
+            _output.WriteLine(doc.ToString());
+        }
+
+        static XDocument BuildSampleData(double errorRate, int count) {
+            Random random = new Random(1);
+            string bad_prefix = @"X";
+
+            XDocument doc = new XDocument(
+            new XDeclaration("1.0", "utf-8", "yes"),
+            new XComment("Sample Data From Who Knows Where"),
+            new XElement("sampleData"));
+
+            for (int i = 0; i < count; i++) {
+                string input = random.Next().ToString();
+                if (random.NextDouble() < errorRate) {
+                    input = bad_prefix + input;
+                }
+                var el = new XElement("item",
+                    new XElement("property", new XAttribute("name", "Cost"), input)
+                );
+                doc.Element("sampleData").Add(el);
+            }
+            return doc;
+        }
+    }
+}
